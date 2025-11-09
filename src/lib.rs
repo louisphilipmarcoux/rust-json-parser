6dd12b97a58ce88ed1b83c9dcd4d7ff82315773e
Louis-Philip Marcoux <louisphilipmarcoux@gmail.com>
2025-11-09 01:39:00 -0500

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -25,9 +25,9 @@
 //! efficient way to parse JSON, especially large files.
 //!
 //! ```no_run
-//! #[allow(clippy::needless_doctest_main)]
 //! use rill_json::{parse_streaming, ParserEvent};
 //!
+//! #[allow(clippy::needless_doctest_main)]
 //! fn main() {
 //!     let json_data = r#"{ "name": "Babbage", "id": 1815 }"#;
 //!     let mut parser = parse_streaming(json_data).unwrap();
